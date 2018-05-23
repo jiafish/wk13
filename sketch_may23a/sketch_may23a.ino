095fd2c70d5f7737a55c09b2b9210d746c9f05af
jiafish <39110691+jiafish@users.noreply.github.com>
2018-05-23 11:20:37 +0800

    Add files via upload

---
+++ sketch_may23a/sketch_may23a.ino
@@ -0,0 +1,31 @@
+const int analogPin = A0;   // the pin that the potentiometer is attached to
+const int ledCount = 10;    // the number of LEDs in the bar graph
+int ledPins[] = {
+  2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+};   // an array of pin numbers to which LEDs are attached
+void setup() {
+  // loop over the pin array and set them all to output:
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    pinMode(ledPins[thisLed], OUTPUT);
+  }
+}
+
+void loop() {
+  // read the potentiometer:
+
+  // loop over the LED array:
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+      digitalWrite(ledPins[thisLed], HIGH); digitalWrite(ledPins[9-thisLed], HIGH);
+      delay(60);
+       digitalWrite(ledPins[thisLed], LOW);digitalWrite(ledPins[9-thisLed], LOW);
+    }
+
+    // if the array element's index is less than ledLevel,
+    // turn the pin for this element on:
+
+    // turn off all pins higher than the ledLevel:
+
+    
+  }
+
+
